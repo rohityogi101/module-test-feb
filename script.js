e8b51465e625d167bfcf6c53efc850b50927a541
rohityogi101 <115226541+rohityogi101@users.noreply.github.com>
2023-03-05 02:42:06 +0530

    change

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
     outputList.innerHTML = ""; //empty html output
     document.body.style.backgroundImage = "url('')";
     searchData = $("#search-box").val();
+    console.log('click par book ka data', searchData);
 
 
     //handling empty search input field
@@ -33,6 +34,7 @@ $(document).ready(function () {
             $("#title").animate({ 'margin-top': '5px' }, 1000); //search box animation
             $(".book-list").css("visibility", "visible");
             displayResults(response);
+            console.log('book ka data', searchData);
             array.push(searchData);
             saveData(array);
           }
@@ -64,7 +66,6 @@ $(document).ready(function () {
       author1 = item.volumeInfo.authors;
       publisher1 = item.volumeInfo.publisher;
       bookLink1 = item.volumeInfo.previewLink;
-      bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
       bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
 
       item2 = response.items[i + 1];
@@ -72,13 +73,12 @@ $(document).ready(function () {
       author2 = item2.volumeInfo.authors;
       publisher2 = item2.volumeInfo.publisher;
       bookLink2 = item2.volumeInfo.previewLink;
-      bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
       bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr;
 
       // in production code, item.text should have the HTML entities escaped.
       outputList.innerHTML += '<div class="row mt-4">' +
-        formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
-        formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+        formatOutput(bookImg1, title1, author1, publisher1) +
+        formatOutput(bookImg2, title2, author2, publisher2) +
         '</div>';
 
       console.log(outputList);
@@ -90,9 +90,9 @@ $(document).ready(function () {
   * @param bookImg title author publisher bookLink
   * @return htmlCard
   */
-  function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+  function formatOutput(bookImg, title, author, publisher) {
     // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
-    var viewUrl = 'book.html?isbn=' + bookIsbn; //constructing link for bookviewer
+    // var viewUrl = 'book.html?isbn=' + bookIsbn; //constructing link for bookviewer
     var htmlCard = `<div class="col-lg-6">
          <div class="card" style="">
            <div class="row no-gutters">
@@ -104,7 +104,6 @@ $(document).ready(function () {
                  <h5 class="card-title">${title}</h5>
                  <p class="card-text">Author: ${author}</p>
                  <p class="card-text">Publisher: ${publisher}</p>
-                 <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
                </div>
              </div>
            </div>
