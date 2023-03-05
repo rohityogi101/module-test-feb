4ee7a84a0103a81c95f2ba0c464aa47439c0f99c
rohityogi101 <115226541+rohityogi101@users.noreply.github.com>
2023-03-05 12:28:46 +0530

    change

--- script1.js
+++ script1.js
@@ -1,18 +1,19 @@
 const item = document.querySelector("#item")
 const toDoBox = document.querySelector("#to-do-box")
 const history = document.querySelector("#history")
+const clear = document.querySelector("#clear")
 
 // Get search data from local storage in form of object
-let objStr = localStorage.getItem("data" || []);
+let objStr = localStorage.getItem("data");
 if (objStr != null) {
   array = JSON.parse(objStr);
 }
 console.log(array);
 
-history.innerHTML = array.map((resp) => {
-  console.log('value of E=', resp);
-  `<p>search= ${resp} </p>`
+toDoBox.innerHTML = array.map((resp) => `<li>${resp} </li>`).join("");
 
+clear.addEventListener('click',()=>{
+  window.localStorage.removeItem("data");
+  location.reload();
 })
 
-history.innerHTML = searchHistory;
